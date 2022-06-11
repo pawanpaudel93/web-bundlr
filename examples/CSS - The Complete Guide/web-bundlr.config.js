77dc27eb007386a09cdf16111684147df32a6129
Pawan Paudel <pawanpaudel93@gmail.com>
2022-06-11 13:19:49 +0545

    added another example uHost

---
+++ examples/CSS - The Complete Guide/web-bundlr.config.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+/** @type {import('web-bundlr').WebBundlrConfig} */
+
+const WebBundlrConfig = {
+  url: 'https://devnet.bundlr.network',
+  currency: 'ethereum',
+  wallet: process.env.WALLET,
+  folderPath: 'uHost',
+  config: {
+    providerUrl: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+  },
+};
+
+module.exports = WebBundlrConfig;
